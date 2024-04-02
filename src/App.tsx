b601f5dab02d94ba5580ccb318266d0a88a44c97
ctcakip1 <tinigaming058@gmail.com>
2024-04-03 00:01:44 +0700

    path alias

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import Portfolio from "./screens/portfolio";
-import "./styles/plugins.scss";
-import "./styles/style.scss";
+import Portfolio from "@/screens/portfolio";
+import "@/styles/plugins.scss";
+import "@/styles/style.scss";
 function App() {
     return <Portfolio />;
 }
