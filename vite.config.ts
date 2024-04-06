72aa88348a56f1f0d7f13cd751dfd921931e5dd6
ctcakip1 <tinigaming058@gmail.com>
2024-04-06 21:19:21 +0700

    done

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig, type PluginOption } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { visualizer } from "rollup-plugin-visualizer";
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    base: "/CV/",
+    plugins: [react(), visualizer() as PluginOption],
     server: {
         port: 3000,
     },
