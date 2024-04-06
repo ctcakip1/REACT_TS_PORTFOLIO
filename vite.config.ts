9bc5bc4bc72816d818a8fe31d7d9d68a2e30f967
ctcakip1 <tinigaming058@gmail.com>
2024-04-06 21:46:09 +0700

    done

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { visualizer } from "rollup-plugin-visualizer";
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/CV/",
+    base: "/",
     plugins: [react(), visualizer() as PluginOption],
     server: {
         port: 3000,
